09b1b3a69e992441d8fafc85b1c30f3f52745c95
Helmut Konrad Fahrendholz <kiwi@derspanier.de>
2019-04-04 18:35:52 +0200

    0.1.0 automatically generated release
    Feature
    
    * add server from baw project (246c48abe013)

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,5 +2,11 @@
 
 Every noteable change is logged here.
 
+## v0.1.0
+
+### Feature
+
+* add server from baw project (246c48abe013)
+
 ## v0.0.0 Initial release
 
--- configo/__init__.py
+++ configo/__init__.py
@@ -13,6 +13,6 @@ import os
 from configo.server import package_address
 from configo.server import package_configuration
 
-__version__ = '0.0.0'
+__version__ = '0.1.0'
 
 ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
