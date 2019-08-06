48182e951559a3dd151f58e61bb66e7a944cbbaf
Helmut Konrad Fahrendholz <kiwi@derspanier.de>
2019-08-06 16:37:00 +0200

    0.1.18 automatically generated release

--- configo/__init__.py
+++ configo/__init__.py
@@ -29,6 +29,6 @@ from configo.share import ready
 from configo.share import share
 from configo.share import todo
 
-__version__ = '0.1.17'
+__version__ = '0.1.18'
 
 ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
