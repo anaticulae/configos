a69de4010d37cd6da057c25f1c9a2018df5e02bf
Helmut Konrad Fahrendholz <helmi3000@outlook.com>
2020-07-01 18:15:26 +0200

    0.6.16 automatically generated release

--- configo/__init__.py
+++ configo/__init__.py
@@ -47,6 +47,6 @@ from configo.holyvalue import load
 from configo.server import package_address
 from configo.server import package_configuration
 
-__version__ = '0.6.15'
+__version__ = '0.6.16'
 
 ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
