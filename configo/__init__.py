4fc79a6f987ed0a11344875d5ac53fbe4cdee9d7
Helmut Konrad Fahrendholz <helmi3000@outlook.com>
2020-09-08 11:47:27 +0200

    0.8.1 automatically generated release

--- configo/__init__.py
+++ configo/__init__.py
@@ -48,6 +48,6 @@ from configo.holyvalue.table import HolyTable
 from configo.server import package_address
 from configo.server import package_configuration
 
-__version__ = '0.8.0'
+__version__ = '0.8.1'
 
 ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
