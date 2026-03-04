5c5b6266fe30dbc2a72f593769ea4049b949454b
Helmut Konrad Schewe <helmutus@outlook.com>
2026-03-04 03:38:02 +0100

    chore(release): 1.0.1

--- configo/__init__.py
+++ configo/__init__.py
@@ -90,7 +90,7 @@ from configo.holyvalue.table import HolyTable
 from configo.server import package_address
 from configo.server import package_configuration
 
-__version__ = '1.0.0'
+__version__ = '1.0.1'
 
 ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 PROCESS = 'configo'
